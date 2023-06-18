ea96cafe7af4fae7b64536a175bc724ec64a84ce
Supriyo Rudra <117836404+supriyorudra@users.noreply.github.com>
2023-06-18 21:21:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ let todoLists = [];
 function addItems(){
 	if (input.value!=="") {
 		todoLists.push(input.value);
-		lists.innerHTML = todoLists.map(list =>{
-			`
-			   <li>list</li>
-		   `
-		})
 	}
 }
 
+lists.innerHTML = todoLists.map(list =>{
+	`
+		<li>${list}</li>
+	`
+})
