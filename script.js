5aab4222251dce63ca0eb0e3bd5322da7b2bfa5d
Supriyo Rudra <117836404+supriyorudra@users.noreply.github.com>
2023-06-18 21:19:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 //your code here
 
+const inputBox = document.getElementById('newTodoInput');
+const lists = document.getElementById('todoList');
+
+let todoLists = [];
+
+function addItems(){
+	if (input.value!=="") {
+		todoLists.push(input.value);
+		lists.innerHTML = todoLists.map(list =>{
+			`
+			   <li>list</li>
+		   `
+		})
+	}
+}
+
